9e4a6fa287bf2662e7032ca71db0eda33f15e622
Pablo Duranti <pduranti@gmail.com>
2011-06-17 12:56:42 -0300

    Divide plugin url mappings from test mappings not exposed

--- grails-app/conf/MLFeUrlMappings.groovy
+++ grails-app/conf/MLFeUrlMappings.groovy
@@ -30,21 +30,5 @@ class MLFeUrlMappings {
 			}
 		}
 		
-		"/$controller/$action?/$id?"{
-			constraints {
-				// apply constraints here
-			}
-		}
-		
-		"/"(view:"/index")
-		
-		"500"(view:'/error')
-		
-		"/test/captcha"(controller:"sample"){
-			action = [GET:"show", POST:"validateCaptcha"]
-		}
-		
-		"/test/tags"(view: "/test/htmltags")
-		
 	}
 }
---
+++ grails-app/conf/UrlMappings.groovy
@@ -0,0 +1,22 @@
+
+class UrlMappings {
+	static mappings = {
+		
+		"/$controller/$action?/$id?"{
+			constraints {
+				// apply constraints here
+			}
+		}
+
+		"/"(view:"/index")
+		
+		"500"(view:'/error')
+		
+		"/test/captcha"(controller:"sample"){
+			action = [GET:"show", POST:"validateCaptcha"]
+		}
+		
+		"/test/tags"(view: "/test/htmltags")
+	}
+	
+}
