6ddca2bcd62ef3e7d52913ec30b66ffb654fb346
Pablo Duranti <pduranti@gmail.com>
2011-08-05 17:19:00 -0300

    Tests for new captcha service

--- test/integration/com/mercadolibre/frontend/services/MLCaptchaServiceTests.groovy
+++ test/integration/com/mercadolibre/frontend/services/MLCaptchaServiceTests.groovy
@@ -1,21 +1,42 @@
 package com.mercadolibre.frontend.services;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.*
 
-import org.junit.Test;
+import org.junit.Test
 
-class MLCaptchaServiceTests {
+class MLCaptchaServiceTests extends GroovyTestCase {
 
 	def mlCaptchaService
+	def captchaStorageServiceStub
 	
 	@Test
-	public void testGetNewChallenge() {
+	public void testDoubleValidationFails() {
 		def captchaKey = mlCaptchaService.getNewChallenge()
+		def word = captchaStorageServiceStub.get(captchaKey)
+		
+		assertTrue mlCaptchaService.isValidAnswer(captchaKey, word)
+		
+		assertFalse mlCaptchaService.isValidAnswer(captchaKey, word) 
 	}
-
+	
 	@Test
-	public void testIsValidAnswer() {
-		fail("Not yet implemented");
+	public void testValidationOK() {
+		def captchaKey = mlCaptchaService.getNewChallenge()
+		def word = captchaStorageServiceStub.get(captchaKey)
+		
+		assertTrue mlCaptchaService.isValidAnswer(captchaKey, word)
+	}
+	
+	@Test
+	public void testDoubleValidationWithRandomCode() {
+		def captchaKey = mlCaptchaService.getNewChallenge()
+		def word = captchaStorageServiceStub.get(captchaKey)
+		
+		assertTrue mlCaptchaService.isValidAnswer(captchaKey, word)
+		
+		def wordWithCode = captchaStorageServiceStub.get(captchaKey)
+		
+		assertTrue mlCaptchaService.isValidAnswer(captchaKey, wordWithCode)
 	}
 
 }
