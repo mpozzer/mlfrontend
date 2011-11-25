407f8f63828cc456d0dbb9db5bab1e42273800e4
Pablo Duranti <pduranti@gmail.com>
2011-11-25 15:30:10 -0200

    Disable most compressions to reduce the risk of breaking the HTML

--- grails-app/services/com/mercadolibre/frontend/services/CompressionService.groovy
+++ grails-app/services/com/mercadolibre/frontend/services/CompressionService.groovy
@@ -38,33 +38,32 @@ class CompressionService {
 	private HtmlCompressor buildHtmlCompressor(){
 
 		HtmlCompressor compressor = new HtmlCompressor();
-		compressor.setEnabled(true);                   //if false all compression is off (default is true)
 		compressor.setRemoveComments(true);            //if false keeps HTML comments (default is true)
-		compressor.setRemoveMultiSpaces(true);         //if false keeps multiple whitespace characters (default is true)
-		compressor.setRemoveIntertagSpaces(true);      //removes iter-tag whitespace characters
-		compressor.setRemoveQuotes(true);              //removes unnecessary tag attribute quotes
-		compressor.setSimpleDoctype(false);             //simplify existing doctype
-		compressor.setRemoveScriptAttributes(true);    //remove optional attributes from script tags
-		compressor.setRemoveStyleAttributes(true);     //remove optional attributes from style tags
-		compressor.setRemoveLinkAttributes(true);      //remove optional attributes from link tags
-		compressor.setRemoveFormAttributes(true);      //remove optional attributes from form tags
-		compressor.setRemoveInputAttributes(true);     //remove optional attributes from input tags
-		compressor.setSimpleBooleanAttributes(true);   //remove values from boolean tag attributes
-		compressor.setRemoveJavaScriptProtocol(true);  //remove "javascript:" from inline event handlers
-		compressor.setRemoveHttpProtocol(false);        //replace "http://" with "//" inside tag attributes
-		compressor.setRemoveHttpsProtocol(false);       //replace "https://" with "//" inside tag attributes
-
 		compressor.setCompressCss(true);               //compress inline css
-		compressor.setCompressJavaScript(false);        //compress inline javascript
-		compressor.setYuiCssLineBreak(8000);             //--line-break param for Yahoo YUI Compressor
-		compressor.setYuiJsDisableOptimizations(true); //--disable-optimizations param for Yahoo YUI Compressor
-		compressor.setYuiJsLineBreak(-1);              //--line-break param for Yahoo YUI Compressor
-		compressor.setYuiJsNoMunge(true);              //--nomunge param for Yahoo YUI Compressor
-		compressor.setYuiJsPreserveAllSemiColons(true);//--preserve-semi param for Yahoo YUI Compressor
-
-		compressor.setGenerateStatistics(false)           // generate statistics
-
-		return compressor;
+		compressor.setEnabled(true);                   //if false all compression is off (default is true)
+//		compressor.setRemoveIntertagSpaces(true);      //removes iter-tag whitespace characters
+//		compressor.setRemoveScriptAttributes(true);    //remove optional attributes from script tags
+//		compressor.setRemoveStyleAttributes(true);     //remove optional attributes from style tags
+//		compressor.setRemoveLinkAttributes(true);      //remove optional attributes from link tags
+//		compressor.setRemoveJavaScriptProtocol(true);  //remove "javascript:" from inline event handlers
+//
+//		compressor.setRemoveMultiSpaces(false);         //if false keeps multiple whitespace characters (default is true)
+//		compressor.setRemoveQuotes(false);              //removes unnecessary tag attribute quotes
+//		compressor.setSimpleDoctype(false);             //simplify existing doctype
+//		compressor.setRemoveFormAttributes(false);      //remove optional attributes from form tags
+//		compressor.setRemoveInputAttributes(false);     //remove optional attributes from input tags
+//		compressor.setSimpleBooleanAttributes(false);   //remove values from boolean tag attributes
+//		compressor.setRemoveHttpProtocol(false);        //replace "http://" with "//" inside tag attributes
+//		compressor.setRemoveHttpsProtocol(false);       //replace "https://" with "//" inside tag attributes
+//
+//		compressor.setCompressJavaScript(false);        //compress inline javascript
+//		compressor.setYuiCssLineBreak(8000);             //--line-break param for Yahoo YUI Compressor
+//		compressor.setYuiJsDisableOptimizations(true); //--disable-optimizations param for Yahoo YUI Compressor
+//		compressor.setYuiJsLineBreak(-1);              //--line-break param for Yahoo YUI Compressor
+//		compressor.setYuiJsNoMunge(true);              //--nomunge param for Yahoo YUI Compressor
+//		compressor.setYuiJsPreserveAllSemiColons(true);//--preserve-semi param for Yahoo YUI Compressor
+//
+//		compressor.setGenerateStatistics(false)           // generate statistics
 
 	}
 }
